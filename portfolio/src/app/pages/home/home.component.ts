28afd72202aafaf4e6e6a27ed119016d0428be43
LuisTorres2122 <ltorresb@miumg.edu.gt>
2023-12-10 19:23:39 -0600

    agrengando diseño responsive

--- portfolio/src/app/pages/home/home.component.ts
+++ portfolio/src/app/pages/home/home.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
 
+  isAnimated = true;
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    const animatedText = this.el.nativeElement.querySelector('#text');
+
+    animatedText.addEventListener('animationend', () => {
+      this.isAnimated = !this.isAnimated;
+      // Vuelve a agregar la clase animada después de un breve período
+      setTimeout(() => {
+        this.isAnimated = !this.isAnimated;
+      }, 3000);
+    });
+  }
 }
